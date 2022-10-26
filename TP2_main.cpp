4c603b02d9567de2802eacffea46713bd8972ca6
ebarcopalacios <113112905+ebarcopalacios@users.noreply.github.com>
2022-10-26 16:23:29 -0300

    Update TP2_main.cpp

--- TP2_main.cpp
+++ TP2_main.cpp
@@ -8,3 +8,28 @@ struct Pizza {
   int cant;
 };
 
+template <typename T> void mostrar(Nodo<T>* lista)
+{
+    while (lista != nullptr) {
+        cout << lista->dato << endl;
+        lista = lista->sig;
+    }
+}
+
+int main()
+{
+    Nodo<int> *pila = nullptr;
+    Nodo<int> *ultpila = pila;
+    push(pila, ultpila, 1);
+    push(pila, ultpila, 2);
+    push(pila, ultpila, 3);
+    cout << "Pila (1 2 3)" << endl;
+    mostrar(pila);
+    cout << endl << "Eliminamos" << endl;
+    while (pila != nullptr)
+        cout << pop(pila, ultpila) << endl;
+    cout << "fin mostrar pila" << endl;
+    if (ultpila == nullptr)
+        cout << "ultpila == nullptr" << endl;
+    cout << "-----------------" << endl;
+}
